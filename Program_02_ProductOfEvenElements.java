427c0965516d6bf16ee6a2cf3afe274556831bd9
PreranaS24 <sheteprernait@gmail.com>
2023-07-18 20:53:09 +0530

    changes in java file

--- Program_02_ProductOfEvenElements.java
+++ Program_02_ProductOfEvenElements.java
@@ -1,27 +1,11 @@
 //WAP to print Product of even elements of Array...
 //
-import java.io.*;
+
 class Program2_ProductOfEvenElements_Array{
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args){
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int size = Integer.parseInt(br.readLine());
-
-		int arr[] = new int[size];
-		int multi = 1;
-
-		for(int i = 0; i<size ; i++){
-
-			arr[i] = Integer.parseInt(br.readLine());
-			if(arr[i]%2 == 0){
-			multi = multi * arr[i];
-			}
-
-		}
-
-		System.out.println(multi);
+			System.out.println("Hello");
 
 		}
 	}
